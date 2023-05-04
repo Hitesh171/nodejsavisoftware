9af1c4e30ba8e535d6344d385720df6dfdd270f9
hitesh_pathania <pathaniahitesh758@gmail.com>
2023-05-04 13:48:11 +0530

    added 4-05-23

---
+++ 4-05-23/parameters and queries/usersex.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const data=require('./data');
+const app=express();
+app.get('/users',(req,res)=>{
+    res.json(data);
+});
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const user=data.find(user=>user.id===Number(id));
+    if(!user){
+        return res.json({msg:"user is not avaliable"});
+    }
+    return res.json(user);
+});
+app.listen(8000,()=>{
+    console.log('successful run')
+});
